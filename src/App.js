e67ee8f978d78e03ff34c80c5187662deb3fcc38
MayTechUg <kimkadu9@gmail.com>
2024-04-27 12:10:18 -0700

    Updated app with new features

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header title = "Facebook"/>
+      <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="*" element={<Missing/>}/>
